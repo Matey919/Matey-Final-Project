f96d4fdf0060288c820bf7b3a7f60737f5366779
mac <mac@heimeis-MacBook-Pro.local>
2025-05-04 22:16:46 -0400

    Revision 1

--- Script.js
+++ Script.js
@@ -2,7 +2,15 @@ const now_playing = document.querySelector('.now-playing');
 const track_art = document.querySelector('.track-art');
 const track_name = document.querySelector('.track-name');
 const track_artist = document.querySelector('.track-artist');
-
+const playpause_btn = document.querySelector('.playpause-track');
+const next_btn = document.querySelector('.next-track');
+const prev_btn = document.querySelector('.prev-track');
+const seek_slider = document.querySelector('.seek_slider');
+const volume_slider = document.querySelector('.volume_slider');
+const curr_time = document.querySelector('.current-time');
+const total_duration = document.querySelector('.total-duration');
+const randomIcon = document.querySelector('.fa-random');
+const curr_track = document.createElement('audio');
 
 // I asked GPT how can I make constant depends on the player's playing situation like which song is playing and if it's being paused or repeat. And It showed me the document. querySelector function that I used a lot
 
