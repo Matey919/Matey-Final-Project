55a792b54d7482b03c6337f8c9da2029a54ba39f
mac <mac@heimeis-MacBook-Pro.local>
2025-05-04 23:10:06 -0400

    Script process 2

--- Script.js
+++ Script.js
@@ -11,8 +11,6 @@ let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
-let randomIcon = document.querySelector('.fa-random');
 let curr_track = document.createElement('audio');
 
 // I asked GPT how can I make constant look at the player's playing situation like which song is playing and if it's being paused or repeat. And It showed me the document. querySelector function that I used a lot
@@ -38,15 +36,41 @@ const music_list = [
         music : 'asset/music/My-Kind-of-Love-_with-Carmody_-_p_.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        img : 'asset/images/sober lorde.jpg',
+        name : 'Sober ',
+        artist : 'Lorde',
+        music : 'asset/music/Lorde-Sober-_p_.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        img : 'asset/images/just wanna forget you.jpg',
+        name : 'Just wanna forget you',
+        artist : 'Maro',
+        music : 'asset/music/MARO-just-wanna-forget-you-_p_.mp3'
     }
 ];
+
+loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
+}
+
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+}
